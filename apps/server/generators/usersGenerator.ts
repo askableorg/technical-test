77091bcbcf40a2254f01c10a0644292b76753555
George Willens <willensgeorge@gmail.com>
2023-09-26 06:44:06 +1000

    Update user generator

--- apps/server/generators/usersGenerator.ts
+++ apps/server/generators/usersGenerator.ts
@@ -1,49 +1,34 @@
-import { faker } from '@faker-js/faker';
-
-type WithId<T> = T & {
-  _id: string;
-};
-
-export type User = WithId<{
-  firstName: string;
-  lastName: string;
-  dp: string;
-  favorites: {
-    lion: string;
-    fish: string;
-  };
-  dob: Date;
-}>;
-
-export type Order = WithId<{
-  product_id: string;
-}>;
+import { faker } from "@faker-js/faker";
+import { User } from "../types";
 
 function createMockUser(): User {
-  return {
-    _id: faker.database.mongodbObjectId(),
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    dp: faker.image.avatar(),
-    dob: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }),
-    favorites: {
-      fish: faker.animal.fish(),
-      lion: faker.animal.lion(),
-    },
-  };
+    return {
+        _id: faker.database.mongodbObjectId(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: "testuser@testemail.com",
+        dp: faker.image.avatar(),
+        dob: faker.date.birthdate({ min: 18, max: 65, mode: "age" }),
+        favorites: {
+            fish: faker.animal.fish(),
+            lion: faker.animal.lion(),
+        },
+        isAdmin: false,
+        password: "testpasswordhash",
+    };
 }
 
 export function generateUserData() {
-  return Array(10000)
-    .fill(null)
-    .reduce<{ users: User[] }>(
-      (acc, _curr) => {
-        const users = createMockUser();
-        acc.users.push(users);
-        return acc;
-      },
-      {
-        users: [],
-      }
-    );
+    return Array(10000)
+        .fill(null)
+        .reduce<{ users: User[] }>(
+            (acc, _curr) => {
+                const users = createMockUser();
+                acc.users.push(users);
+                return acc;
+            },
+            {
+                users: [],
+            }
+        );
 }

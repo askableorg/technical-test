7aba48abc89d3eeba4e12178f653c2427e247518
George Willens <willensgeorge@gmail.com>
2023-09-26 06:49:06 +1000

    Add unauthorised page

---
+++ apps/frontend/src/components/auth/Unauthorised.tsx
@@ -0,0 +1,10 @@
+const UnauthorisedPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Unauthorised</h1>
+            <p>You do not have permission to access this page.</p>
+        </div>
+    );
+};
+
+export default UnauthorisedPage;
